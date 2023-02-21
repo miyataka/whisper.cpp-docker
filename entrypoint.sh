fe2a5a90fd23542ebda952060cdca5a97ae9a6e7
Takayuki Miyahara <voyager.3taka28@gmail.com>
2023-02-21 17:26:29 +0900

    feat: output to csv

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ LANG=$2
 
 ffmpeg -i $INPUT_MP4_FILE_PATH -ar 16000 samples/test.wav
 
-./main -m models/ggml-medium.bin -l $LANG -f samples/test.wav
+./main -m models/ggml-medium.bin --output-csv --output-file ./mnt/output -l $LANG -f samples/test.wav
